30e43eabac3899b357f8a1c2aaa8f78699f9d606
niimf <75733381+niimf@users.noreply.github.com>
2024-04-19 10:58:42 -0300

    Update cadastroPG.php
    front feito

--- cadastroPG.php
+++ cadastroPG.php
@@ -1,10 +1,30 @@
-<h1>Cadastrar Usuário</h1>
-<p>Selecione o tipo de conta:</p>
-<tr>
-    <td>
-        <a href="cadastrarBABA1.php">[ BABÁ ] </a>
-        <a href="cadastrarPAIS1.php">[ PAIS ]</a>
-    </td>
-        
-</tr>
-<a href="index.php"><button>Voltar</button></a>
+<html>
+    <head>
+        <title>Cadastro</title>
+        <link rel="stylesheet" href="css\stylePG.css">
+    </head>
+    <body>
+        <header>
+            <a href="index.php"><img src="imagem/bbbyy.png" width="120px" ></a>
+            <img src="imagem\nome.jpg" width="250px">
+        </header>
+        <main>
+            <section class="cadastro">
+                <div class="cadastro-baba">
+                    <img class="icon" src="imagem\babaicon.png"></img>
+                    <h1>Seja uma Babá</h1>
+                    <p>Ao criar seu perfil, você poderá receber propostas de trabalho de pais/responsáveis que necessitam da sua ajuda!</p>
+                    <a href="cadastrarBABA1.php"><button class="btn">Cadastrar Babá</button></a>
+                </div>
+            </section> 
+            <section class="cadastro">
+                <div class="cadastro-pais">
+                    <img class="icon" src="imagem\paisicon.png"></img>
+                    <h1>Seja um Pai</h1>
+                    <p>Ao criar seu perfil, você terá acesso ao perfil de diversas babás para te ajudar!</p>
+                    <a href="cadastrarPAIS1.php"><button class="btn">Cadastrar Pais</button></a>
+                </div>
+            </section>
+        </main>
+    </body> 
+</html>
