2987854c6490bd1fa851930836fd0fba997f1851
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-20 19:52:26 -0300

    Update cadastrarPAIS2.php

--- cadastrarPAIS2.php
+++ cadastrarPAIS2.php
@@ -1,20 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro PAIS2</title>
+    <link rel="stylesheet" href="css\cadastrarPAIS2.css">
+</head>
+<body>
+    
+<div class="cabecalho">
 <h1>Cadastrar Pais</h1>
-<p>Dados Pais</p>
+</div>
+
 <form method="POST" action="cadastrar_actP2.php">
     <ul>
+        <div class="caixa">
     <label>
         <input type="hidden" name="Conta" value="PAIS" />
         <input type="hidden" name="fk_idUsuario" value="<?php echo $_GET['idUsuario'];?>" />
     </label>
     <label>
-        <li>Endereço: <input type="text" name="endereco" pattern="[a-zA-Z0-9\s,'-]*" required title="Por favor, insira um endereço válido"/>
+        <li><strong>Endereço: </strong><input type="text" name="endereco" pattern="[a-zA-Z0-9\s,'-]*" required title="Por favor, insira um endereço válido"/>
     </label>
     <label>
-        <li>Quantidade de criança: <input type="text" name="qtdeCrianca" pattern="[0-9]+" title="Insira apenas números." required/>
+        <li><strong>Quantidade de criança:</strong> <input type="text" name="qtdeCrianca" pattern="[0-9]+" title="Insira apenas números." required/>
     </label>
     <label>
-        <li>Descrição da família: <input type="text" name="descricao" title="Detalhe um pouco sobre como é sua família." required/>
+        <li><strong>Descrição da família:</strong> <input type="text" name="descricao" title="Detalhe um pouco sobre como é sua família." required/>
+    
     </label>
     </ul>
-    <input type="submit" onclick="alert('Cadastro PAIS feito!')" value="Salvar"/>
+    <input type="submit" id="button" onclick="alert('Cadastro PAIS feito!')" value="Salvar"/>
+        </div>
 </form>
+</body>
+</html>
