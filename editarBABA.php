12b5c93acf287a3cc92d6bbfd93066b247f57bef
Ana Paula Militao <anapaulammilitao@hotmail.com>
2024-04-21 09:06:59 -0300

    alteração nas telas de edição de BABÁ, PAIS e USUÁRIOS

--- editarBABA.php
+++ editarBABA.php
@@ -3,33 +3,80 @@ require 'configu.php';
 
 $baba = [];
 $idBaba = filter_input(INPUT_GET, 'idBaba');
-if($idBaba){
+if ($idBaba) {
     $sql = $pdo->prepare("SELECT * FROM baba WHERE idBaba = :idBaba");
     $sql->bindValue(':idBaba', $idBaba);
     $sql->execute();
 
-    if($sql->rowCount() > 0){
+    if ($sql->rowCount() > 0) {
         $baba = $sql->fetch(PDO::FETCH_ASSOC);
-    }else{
+    } else {
         header("Location: selectBABA.php");
         exit;
     }
-}else{
+} else {
     header("Location: selectBABA.php");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<h1>Editar Babá</h1>
-<form method="POST" action="editarBABA_act.php">
-    <ul>
-    <input type="hidden" name= "idBaba" value="<?=$baba['idBaba'];?>"/>
-    <label>
-        <li>Sobre: <input type="text" name="sobre" title="Descreva um pouco sobre você e suas experiências." required value="<?=$baba['sobre'];?>"/>
-    </label>
-    <label>
-        <li>Valor/hora: <input type="text" name="valorH" pattern="\d+(\.\d+)?" required title="Insira um valor em reais (com ponto ao invés de vírgula!)" value="<?=$baba['valorH'];?>"/>
-    </label>
-    <input type="submit" value="Atualizar"/>
-    </ul>
-</form>
-<a href="selectBABA.php"><button>Voltar</button></a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\editarBABA.css">
+    <!-- <link rel="stylesheet" type="text/css" href="selebaba.css"> -->
+    <title>Editar Baba</title>
+</head>
+<a href="selectBABA.php" class="voltar">Voltar</a>
+<body>
+    <h1>Editar Babá</h1>
+    <div class="formulario">
+        <form method="POST" action="editarBABA_act.php">
+            <ul>
+            <div class="caixa">
+                <input type="hidden" name="idBaba" value="<?= $baba['idBaba']; ?>" />
+                <label>
+                    <strong>Sobre Mim</strong><input type="text" name="sobre" title="Descreva um pouco sobre você e suas experiências."
+                            required value="<?= $baba['sobre']; ?>" />
+                </label>
+                <label>
+        <strong>Referência (Telefone):</strong> <input type="text" name="telefone" pattern="\d{2}\s?\d{4,5}-?\d{4}" title="Formato: (XX) XXXX-XXXX ou (XX) XXXXX-XXXX." required/>
+        </label>
+                <label>
+                    <strong>Valor/hora</strong><input type="text" name="valorH" pattern="\d+(\.\d+)?" required
+                            title="Insira um valor em reais (com ponto ao invés de vírgula!)"
+                            value="<?= $baba['valorH']; ?>" />
+                </label>
+                <label>
+                    <strong>Tempo de Experiência</strong><input type="text" name="experiencia" title="Descreva seu tempo de experiência."
+                            required value="<?= $baba['sobre']; ?>" />
+                </label>
+            </div>
+            </ul>
+            <div class="btn_atualizar">
+                <input type="submit" id="button" value="Atualizar" />
+            </div>
+        </form>
+    </div>
+</body>
+<style>
+    a.voltar {
+    position: absolute;
+    top: 55px;
+    left: 29px;
+    background-color: #ac9e9d;
+    color: white;
+    padding: 0px 20px;
+    text-align: center;
+    line-height: 35px;
+    text-decoration: none;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 8px;
+
+}
+</style>
+</html>
+
+
--- editarPAIS.php
+++ editarPAIS.php
@@ -3,36 +3,61 @@ require 'configu.php';
 
 $pais = [];
 $idPais = filter_input(INPUT_GET, 'idPais');
-if($idPais){
+if ($idPais) {
     $sql = $pdo->prepare("SELECT * FROM pais WHERE idPais = :idPais");
     $sql->bindValue(':idPais', $idPais);
     $sql->execute();
 
-    if($sql->rowCount() > 0){
+    if ($sql->rowCount() > 0) {
         $pais = $sql->fetch(PDO::FETCH_ASSOC);
-    }else{
+    } else {
         header("Location: select.php");
         exit;
     }
-}else{
+} else {
     header("Location: select.php");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<h1>Editar Pais</h1>
-<form method="POST" action="editarPAIS_act.php">
-    <ul>
-    <input type="hidden" name= "idPais" value="<?=$idPais;?>"/>
-    <label>
-        <li>Endereço: <input type="text" name="endereco" pattern="[a-zA-Z0-9\s,'-]*" required title="Por favor, insira um endereço válido" value="<?=$pais['endereco'];?>"/>
-    </label>
-    <label>
-        <li>Qtde Criança: <input type="number" name="qtdeCrianca" value="<?=$pais['qtdeCrianca'];?>"/>
-    </label>
-    <label>
-        <li>Descriação: <input type="text" name="descricao" title="Detalhe um pouco sobre como é sua família." required value="<?=$pais['descricao'];?>"/>
-    </label>
-    <input type="submit" value="Atualizar"/>
-    </ul>
-</form>
-<a href="selectPAIS.php"><button>Voltar</button></a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\editarPAIS.css">
+    <title>Editar Pais</title>
+</head>
+
+<body>
+    <div class="cabecalho">
+        <h1>Editar Pais</h1>
+        <br>
+    </div>
+    <div class="formulario">
+        <form method="POST" action="editarPAIS_act.php">
+            <ul>
+            <div class="caixa">
+                <input type="hidden" name="idPais" value="<?= $idPais; ?>" />
+                <label>
+                   <strong>Endereço:</strong> <input type="text" name="endereco" pattern="[a-zA-Z0-9\s,'-]*" required
+                            title="Por favor, insira um endereço válido" value="<?= $pais['endereco']; ?>" />
+                </label>
+                <label>
+                    <strong>Qtde Criança:</strong> <input type="number" name="qtdeCrianca" value="<?= $pais['qtdeCrianca']; ?>" />
+                </label>
+                <label>
+                    <strong>Descriação:</strong> <input type="text" name="descricao"
+                            title="Detalhe um pouco sobre como é sua família." required
+                            value="<?= $pais['descricao']; ?>" />
+                </label>
+            </div>
+            <div class="btn_atualizar">
+                <input type="submit" id="button" value="Atualizar" />
+            </div>
+            </ul>
+        </form>
+    </div>
+    <a href="selectPAIS.php" class="voltar">Voltar</a>
+</body>
+
+</html>
